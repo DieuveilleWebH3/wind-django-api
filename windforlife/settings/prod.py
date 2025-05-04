0a65dd55ec3acca4f6de03e3ffe7b09548707fcd
DieuveilleWebH3 <d.boussaellenga@h3hitema.fr>
2025-05-04 14:15:54 +0200

    Designing the project architecture to be prod ready

---
+++ windforlife/settings/prod.py
@@ -0,0 +1,19 @@
+from windforlife.settings.base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
+}
