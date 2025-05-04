fa385fc53f265fc5e9e1a55a31ffb994d2c53d83
DieuveilleWebH3 <d.boussaellenga@h3hitema.fr>
2025-05-04 16:11:26 +0200

    Updated dev settings to handle django debug toolbar

--- windforlife/settings/dev.py
+++ windforlife/settings/dev.py
@@ -1,7 +1,13 @@
 from windforlife.settings.base import *
+import socket
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", False)
+
+if DEBUG:
+    # Detect internal IPs for Docker
+    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+    INTERNAL_IPS = ["127.0.0.1", "localhost"] + [ip[:-1] + "1" for ip in ips]
 
 ALLOWED_HOSTS = [
     "localhost",
